5f2be3879e97c21519d8b49b6169ff58085abce3
Luiz Abrahao <luiz.abrahao@eukleia-training.com>
2012-10-30 16:34:30 +0000

    Ant interface now documents 8 directions instead of 4

--- src/main/java/org/ag/ants/agent/Ant.java
+++ src/main/java/org/ag/ants/agent/Ant.java
@@ -7,61 +7,69 @@ import org.ag.common.env.Direction;
 import org.ag.common.env.Node;
 
 /**
- * Defines the basic API that must be implemented by any agent that represent
- * a ant.
+ * Defines the basic API that must be implemented by any agent that represent a
+ * ant.
  * 
  * @author Luiz Abrahao <luiz@luizabrahao.com>
- *
+ * 
  */
 public interface Ant {
 	/**
 	 * Each agent moves towards to a direction, this effectively means that the
 	 * ant has a bigger probability of selecting a neighbour node on that
 	 * direction than in any other.
-	 *   
+	 * 
 	 * @return Direction direction the ant is moving towards to.
 	 */
 	Direction getMovingDirection();
-	
+
 	/**
 	 * Changes the direction the agent is moving towards to.
-	 * @param movingDirection Direction the agent is more likely to follow. 
+	 * 
+	 * @param movingDirection
+	 *            Direction the agent is more likely to follow.
 	 */
 	void setMovingDirection(Direction movingDirection);
-	
+
 	/**
-	 * An agent has a map of ChemicalStimulusType and how much they increment 
+	 * An agent has a map of ChemicalStimulusType and how much they increment
 	 * each stimulus they are sensitive to. This method looks up that list and
 	 * increment the node's current chemical stimulus accordingly to each agent
-	 * type. 
+	 * type.
 	 * 
-	 * @param chemicalCommStimulusType Chemical stimulus that will be updated.
+	 * @param chemicalCommStimulusType
+	 *            Chemical stimulus that will be updated.
 	 */
-	void incrementStimulusIntensity(ChemicalCommStimulusType chemicalCommStimulusType);
-	
+	void incrementStimulusIntensity(
+			ChemicalCommStimulusType chemicalCommStimulusType);
+
 	/**
 	 * Collects food from a food source.
 	 * 
-	 * @param foodSource Food source that the food will be collected from
-	 * @param amountToCollect Amount of food the agent will try to collect.
+	 * @param foodSource
+	 *            Food source that the food will be collected from
+	 * @param amountToCollect
+	 *            Amount of food the agent will try to collect.
 	 * @return Double the amount of food the agent actually collected.
 	 */
 	double collectFood(Agent foodSource, double amountToCollect);
-	
+
 	/**
 	 * Checks if the agent is caring food.
+	 * 
 	 * @return boolean True if the agent is caring food, false otherwise.
 	 */
 	boolean isCarryingFood();
-	
+
 	/**
 	 * Each agent should have a short term memory of where the agent has been.
 	 * This method adds a new method to the agent's memory.
 	 * 
-	 * @param node Node where the agent has visited.
+	 * @param node
+	 *            Node where the agent has visited.
 	 */
 	void addToMemory(Node node);
-	
+
 	/**
 	 * Scans all agents in the current node, if any agent that is a food source
 	 * is found, it is returned. If there are more than one food sources in the
@@ -72,14 +80,14 @@ public interface Ant {
 	 * @return FoodSourceAgent food source
 	 */
 	FoodSourceAgent findFoodSource();
-	
+
 	/**
 	 * Returns the most recent node that the agent contains in his memory.
 	 * 
 	 * @return Node Latest node visited.
 	 */
 	Node getLatestNodeFromMemory();
-	
+
 	/**
 	 * Sometimes a agent needs to reinforce something that it has learned, a way
 	 * to do that is to deposit big quantities of chemical stimulus to tell it
@@ -91,28 +99,36 @@ public interface Ant {
 	 * So this method deposits the normal amount of stimulus multiplied by the
 	 * factor that is passed as parameter.
 	 * 
-	 * @param chemicalCommStimulusType Stimulus to be deposited
-	 * @param factor Factor that the stimulus increment will be multiplied by
+	 * @param chemicalCommStimulusType
+	 *            Stimulus to be deposited
+	 * @param factor
+	 *            Factor that the stimulus increment will be multiplied by
 	 */
-	void incrementStimulusIntensityMultipliedByFactor(final ChemicalCommStimulusType chemicalCommStimulusType, int factor);
-	
+	void incrementStimulusIntensityMultipliedByFactor(
+			final ChemicalCommStimulusType chemicalCommStimulusType, int factor);
+
 	/**
 	 * Inverts the agents moving direction. If the agent is moving NORTH it
 	 * should be moving SOUTH after this method is called. The same for all the
 	 * other directions:
 	 * 
 	 * NORTH -> SOUTH
-	 * SOUTH -> NORTH
+	 * NOTH_EAST -> SOUTH_WEST
 	 * EAST -> WEST
+	 * SOUT_EAST -> NORTH_WEST
+	 * SOUTH -> NORTH
+	 * SOUTH_WEST -> NORTH_EAST
 	 * WEST -> EAST
+	 * NORTH_WEST -> SOUTH_EAST
 	 */
 	void invertDirection();
-	
+
 	/**
 	 * If an agent is caring food, it will be able to deposit the food is caring
 	 * in a nest.
 	 * 
-	 * @param nest AntNestAgent Nest the food will be deposited.
+	 * @param nest
+	 *            AntNestAgent Nest the food will be deposited.
 	 */
 	void depositFood(AntNestAgent nest);
 }
